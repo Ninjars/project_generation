1999e460ca65dbfae27f5681cf04ed2057d111fa
Jez <jez0707@gmail.com>
2016-12-10 22:54:34 +0000

    Edges of extrusion now meet up.  Added random variance to depth of extrusion.

--- Assets/Scripts/WallGenExtrusion.cs
+++ Assets/Scripts/WallGenExtrusion.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class WallGenExtrusion : MonoBehaviour, Interfaces.IWallGenerator {
     public float wallHeight = 5;
-    public float wallDepth = 1;
+    public float wallDepthVariance = 0.25f;
+    public float minWallDepth = 0.5f;
+
+    private static System.Random random = new System.Random();
 
     public Mesh generate(List<List<int>> outlines, List<Vector3> vertices) {
         List<Vector3> wallVertices = new List<Vector3>();
         List<int> wallTriangles = new List<int>();
         foreach (List<int> outline in outlines) {
             for (int i = 0; i < outline.Count - 1; i++) {
-                List<Vector3[]> outlineBox = makeBoxFromLine(vertices[outline[i]], vertices[outline[i + 1]]);
+                int previousVertex = i == 0 ? outline.Count - 1 : i - 1;
+                int nextVertex = i == outline.Count - 2 ? 0 : i + 2;
+                List<Vector3[]> outlineBox = makeBoxFromLine(vertices[outline[i]], vertices[outline[i + 1]], vertices[outline[previousVertex]], vertices[outline[nextVertex]]);
 
                 foreach (Vector3[] surface in outlineBox) {
                     int startIndex = wallVertices.Count;
@@ -32,17 +37,24 @@ public class WallGenExtrusion : MonoBehaviour, Interfaces.IWallGenerator {
         return new int[] { offset+a, offset+b, offset+d, offset+a, offset+d, offset+c };
     }
 
-    private List<Vector3[]> makeBoxFromLine(Vector3 topLeft, Vector3 topRight) {
+    private List<Vector3[]> makeBoxFromLine(Vector3 topLeft, Vector3 topRight, Vector3 neighbourLeft, Vector3 neighbourRight) {
         Vector3 bottomLeft = topLeft - Vector3.up * wallHeight;
         Vector3 bottomRight = topRight - Vector3.up * wallHeight;
 
+        // find normal of adjacent line
+        Vector3 normalPrevious = getNormal(neighbourLeft, topLeft, bottomLeft);
+        Vector3 normalNext = getNormal(topRight, neighbourRight, bottomRight);
+
+        float offset = minWallDepth + (float) random.NextDouble() * wallDepthVariance;
+
         // surface is made of 2 triangles, but we are assuming that it is planar and so both share same normal
         Vector3 normal = getNormal(topLeft, topRight, bottomLeft);
-        Vector3 offset = normal * wallDepth;
-        Vector3 topLeftOffset = topLeft + offset;
-        Vector3 topRightOffset = topRight + offset;
-        Vector3 bottomLeftOffset = bottomLeft + offset;
-        Vector3 bottomRightOffset = bottomRight + offset;
+        Vector3 offsetLeft = Vector3.Normalize(normal + normalPrevious) * offset;
+        Vector3 offsetRight = Vector3.Normalize(normal + normalNext) * offset;
+        Vector3 topLeftOffset = topLeft + offsetLeft;
+        Vector3 topRightOffset = topRight + offsetRight;
+        Vector3 bottomLeftOffset = bottomLeft + offsetLeft;
+        Vector3 bottomRightOffset = bottomRight + offsetRight;
 
         List<Vector3[]> box = new List<Vector3[]>();
         // back
