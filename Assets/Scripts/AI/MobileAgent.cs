1ed704bc3784344d7a054f10c9259dc38cc4ad43
Jeremy Stevens <jeremy@touchsurgery.com>
2017-01-26 21:06:37 +0000

    wip navigation collisions

--- Assets/Scripts/AI/MobileAgent.cs
+++ Assets/Scripts/AI/MobileAgent.cs
@@ -9,10 +9,14 @@ public abstract class MobileAgent : BaseAgent {
     private float terminalSpeed;
     private Vector3 currentVelocity = Vector3.zero;
     private float decelerationBuffer = 1f;
+    private BoxCollider midCollider;
+    private BoxCollider farCollider;
 
     protected void start(float speed) {
         setSpeed(speed);
         GlobalRegister.addMobileAgent(this);
+        midCollider = gameObject.AddComponent<BoxCollider>();
+        farCollider = gameObject.AddComponent<BoxCollider>();
     }
 
     public void setSpeed(float val) {
@@ -39,6 +43,9 @@ public abstract class MobileAgent : BaseAgent {
         // todo: implement actual path finding algo!
         
         Vector3.ClampMagnitude(currentVelocity, terminalSpeed);
+
+        updateColliders(currentVelocity);
+
         transform.position += currentVelocity * Time.fixedDeltaTime;
 
         if (dist <= nextAction.getMaxTriggerRange()) {
@@ -47,6 +54,42 @@ public abstract class MobileAgent : BaseAgent {
             return true;
         } else {
             return false;
+        }
+    }
+
+    private void updateColliders(Vector3 velocity) {
+        float magnitude = velocity.magnitude;
+        midCollider.size.Set(1, 1, magnitude / 2);
+        midCollider.center.Set(0.5f, 0.5f, magnitude / 4f);
+        farCollider.size.Set(1, 1, magnitude / 2);
+        farCollider.center.Set(0.5f, 0.5f, magnitude * 3 / 4f);
+    }
+
+    private List<MobileAgent> getPotentialCollidingAgents() {
+        Vector3 scanOffset = gameObject.transform.position + currentVelocity;
+        Collider[] foundObjects = Physics.OverlapSphere(scanOffset, scanOffset.magnitude);
+        List<MobileAgent> collidingAgents = new List<MobileAgent>();
+        foreach (Collider hit in foundObjects) {
+            Transform parent = hit.transform.parent;
+            if (parent == null) {
+                continue;
+            }
+            MobileAgent agent = parent.gameObject.GetComponent<MobileAgent>();
+            if (agent != null && onCollisionCourse(agent)) {
+                collidingAgents.Add(agent);
+            }
+        }
+        return collidingAgents;
+    }
+
+    private bool onCollisionCourse(MobileAgent other) {
+
+        return false;
+    }
+
+    void OnCollisionStay(Collision collisionInfo) {
+        foreach (ContactPoint contact in collisionInfo.contacts) {
+            Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
         }
     }
 }
