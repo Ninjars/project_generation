24de08a4d44611f5cfab020f334dfe9a24017860
Jez <jez0707@gmail.com>
2017-02-11 11:44:42 +0000

    initial pathway commit

---
+++ Assets/Scripts/PathGen/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public List<GameObject> linkToNodesObjs;
+    private List<Node> knownNodes;
+
+    void Awake () {
+        knownNodes = new List<Node>();
+        foreach (GameObject node in linkToNodesObjs) {
+            Node nodeComponent = node.GetComponent<Node>();
+            if (nodeComponent != null) {
+                knownNodes.Add(nodeComponent);
+            } else {
+                Debug.LogWarning("Node.linkToNodesObjs contained a gameobject with no Node component");
+            }
+        }
+    }
+	
+	void Update () {
+		
+	}
+
+    public List<Node> getKnownNodes() {
+        return knownNodes;
+    }
+}
---
+++ Assets/Scripts/PathGen/NodeTree.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeTree : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
