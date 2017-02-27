dcebef170f933a38d4e87efb97fccf3e7d559b29
Jeremy Stevens <jeremy@touchsurgery.com>
2017-02-27 20:41:34 +0000

    Implemented NodeRangeIndicator, to be used to draw range circles around nodes!

---
+++ Assets/Scripts/PathGen/NodeRangeIndicator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class NodeRangeIndicator : MonoBehaviour {
+    [Range(0.1f, 100f)]
+    public float radius = 1.0f;
+
+    [Range(3, 256)]
+    public int numSegments = 128;
+
+    void Start() {
+        render();
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+        render();
+    }
+
+    private void render() {
+        LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.numPositions = numSegments + 1;
+        lineRenderer.useWorldSpace = false;
+
+        float deltaTheta = (float) (2.0 * Mathf.PI) / numSegments;
+        float theta = 0f;
+
+        for (int i = 0 ; i < numSegments + 1 ; i++) {
+            float x = radius * Mathf.Cos(theta);
+            float z = radius * Mathf.Sin(theta);
+            Vector3 pos = new Vector3(x, 0, z);
+            lineRenderer.SetPosition(i, pos);
+            theta += deltaTheta;
+        }
+    }
+}
