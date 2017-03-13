993c2cd12203e12ec38165188f67651133f5f8b2
Jeremy Stevens <jeremy@touchsurgery.com>
2017-03-13 20:22:24 +0000

    refactored NodeInteractionManager to allow for multiple interaction types, CONNECT (the previous behaviour) and MOVE (to be implemented)

--- Assets/Scripts/NodeSystem/NodeInteractionManager.cs
+++ Assets/Scripts/NodeSystem/NodeInteractionManager.cs
@@ -5,69 +5,110 @@ using UnityEngine;
 namespace Node {
     public class NodeInteractionManager : MonoBehaviour {
 
-        private Node mFirstNode = null;
-        private Plane mBaseCollisionPlane;
-
-        public void onInteraction(Node node) {
-            Debug.Log("NodeInteractionManager: onInteraction() " + node);
-            if (node == null) {
-                clearInteraction();
-            } else if (mFirstNode == null) {
-                beginInteraction(node);
-            } else {
-                endInteraction(node);
-            }
+        public enum InteractionMode {
+            CONNECT,
+            MOVE
         }
 
-        private void beginInteraction(Node startNode) {
-            mFirstNode = startNode;
-        }
-
-        private void clearInteraction() {
-            mFirstNode = null;
-        }
-
-        private void endInteraction(Node endNode) {
-            Debug.Assert(mFirstNode != null);
-            if (mFirstNode.hasConnection(endNode)) {
-                Debug.Log("NodeInteractionManager: removing connection " + mFirstNode + ", " + endNode);
-                mFirstNode.removeConnection(endNode);
-                endNode.removeConnection(mFirstNode);
-            } else {
-                Debug.Log("NodeInteractionManager: adding connection " + mFirstNode + ", " + endNode);
-                mFirstNode.addConnection(endNode);
-                endNode.addConnection(mFirstNode);
-            }
-            clearInteraction();
-        }
+        private Node selectedNode = null;
+        private Plane baseCollisionPlane;
+        private InteractionMode mode = InteractionMode.CONNECT;
 
         void Awake() {
-            mBaseCollisionPlane = new Plane(Vector3.up, Vector3.zero);
+            baseCollisionPlane = new Plane(Vector3.up, Vector3.zero);
         }
 
         void Update() {
             if (Input.GetMouseButtonDown(0)) {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                float distance;
-                int mask = 1 << LayerMask.NameToLayer("nodes");
-                RaycastHit hit;
-                Physics.Raycast(ray, out hit, Mathf.Infinity, mask);
-                if (hit.collider != null) {
-                    Debug.Log("NodeInteractionManager: click on gameobject: " + hit.collider.gameObject);
-                    Node selectedNode = hit.collider.transform.parent.gameObject.GetComponent<Node>();
-                    Debug.Assert(selectedNode != null);
-                    if (mFirstNode == null) {
-                        beginInteraction(selectedNode);
-                    } else {
-                        endInteraction(selectedNode);
-                    }
-                } else {
-                    if (mBaseCollisionPlane.Raycast(ray, out distance)) {
-                        Vector3 hitPoint = ray.GetPoint(distance);
-                        Debug.Log("NodeInteractionManager: click at position: " + hitPoint);
-                        clearInteraction();
-                    }
+                checkForNodeInteraction(ray);
+            }
+        }
+
+        public void setInteractionMode(InteractionMode mode) {
+            Debug.Log("NodeInteractionManager: setInteractionMode() " + mode);
+            clearInteraction();
+            this.mode = mode;
+        }
+
+        private void clearInteraction() {
+            Debug.Log("NodeInteractionManager: clearInteraction()");
+            selectedNode = null;
+        }
+
+        private bool checkForNodeInteraction(Ray ray) {
+            float distance;
+            int mask = 1 << LayerMask.NameToLayer("nodes");
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit, Mathf.Infinity, mask);
+            if (hit.collider != null) {
+                Debug.Log("NodeInteractionManager: click on gameobject: " + hit.collider.gameObject);
+                Node selectedNode = hit.collider.transform.parent.gameObject.GetComponent<Node>();
+                return onInteraction(selectedNode);
+
+            } else {
+                clearInteraction();
+                if (baseCollisionPlane.Raycast(ray, out distance)) {
+                    Vector3 hitPoint = ray.GetPoint(distance);
+                    Debug.Log("NodeInteractionManager: click at position: " + hitPoint);
                 }
+                return false;
+            }
+        }
+
+        private bool onInteraction(Node node) {
+            Debug.Log("NodeInteractionManager: onInteraction() " + node);
+            if (node == null) {
+                clearInteraction();
+                return false;
+            } else if (selectedNode == null) {
+                beginInteraction(node);
+            } else {
+                endInteraction(node);
+            }
+            return true;
+        }
+
+        private void beginInteraction(Node node) {
+            Debug.Log("NodeInteractionManager: beginInteraction()");
+            selectedNode = node;
+        }
+
+        private void endInteraction(Node node) {
+            Debug.Log("NodeInteractionManager: endInteraction()");
+            Debug.Assert(selectedNode != null);
+            switch (mode) {
+                case InteractionMode.CONNECT: {
+                    connectionInteraction(node);
+                    break;
+                }
+                case InteractionMode.MOVE: {
+                    moveInteraction(node);
+                    break;
+                }
+            }
+            clearInteraction();
+        }
+
+        private void connectionInteraction(Node node) {
+            Debug.Log("NodeInteractionManager: connectionInteraction()");
+            if (selectedNode.hasConnection(node)) {
+                Debug.Log("NodeInteractionManager: removing connection " + selectedNode + ", " + node);
+                selectedNode.removeConnection(node);
+                node.removeConnection(selectedNode);
+            } else {
+                Debug.Log("NodeInteractionManager: adding connection " + selectedNode + ", " + node);
+                selectedNode.addConnection(node);
+                node.addConnection(selectedNode);
+            }
+        }
+
+        private void moveInteraction(Node node) {
+            Debug.Log("NodeInteractionManager: moveInteraction()");
+            if (selectedNode.hasConnection(node)) {
+                Debug.Log("NodeInteractionManager: moving " + selectedNode + " to " + node);
+            } else {
+                Debug.Log("NodeInteractionManager: invalid move; no connection from " + selectedNode + " to " + node);
             }
         }
     }
