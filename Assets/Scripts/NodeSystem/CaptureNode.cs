0707f98e90fce30943ec81a3779813dcbbe38e90
Jez <jeremy@jeremystevens.net>
2017-05-01 22:15:57 +0100

    Implemented outline CaptureNode, currently just as an implementation of the CompoundValue GameNode

---
+++ Assets/Scripts/NodeSystem/CaptureNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Node {
+    public class CaptureNode : GameNodeCompoundValue {
+
+
+        public override void onPacket(Packet packet) {
+            bool ownerMatches = packet.getOwnerId() == getOwnerId();
+            if (ownerMatches) {
+                changeValue(1);
+            } else {
+                if (currentValue == 0) {
+                    nodeComponent.removeAllConnections();
+                    setOwnerId(packet.getOwnerId());
+                } else {
+                    changeValue(-1);
+                }
+            }
+            nodeUi.hasUpdate();
+        }
+    }
+}
