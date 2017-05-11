ac3fb3850bf9fcb4cc859d256ddefe5579fa2da3
Jez <jeremy@jeremystevens.net>
2017-05-11 22:03:06 +0100

    Implemented scaling system for EmitterNode's rate of emissions to allow them to retain a reserve and to reward building up a reserve with more rapid emission

--- Assets/Scripts/NodeSystem/EmitterNode.cs
+++ Assets/Scripts/NodeSystem/EmitterNode.cs
@@ -1,7 +1,16 @@
-﻿namespace Node {
+﻿using UnityEngine;
+
+namespace Node {
     public class EmitterNode : GameNodeEmitter {
 
+        [Tooltip("Each hostile packet received will add one lock that is cleared every medium beat, up to this many locks")]
         public int maxMediumBeatLockDuration = 3;
+        [Range(0f, 1f)]
+        [Tooltip("% above which emitter will emit at fastest speed")]
+        public float fastThreshold = 0.66f;
+        [Range(0f, 1f)]
+        [Tooltip("% above which emitter will emit at medium speed")]
+        public float mediumThreshold = 0.33f;
 
         private int currentLocks = 0;
 
@@ -27,14 +36,32 @@
             }
         }
 
+        private bool isEmittingSlow() {
+            return currentValue / (float)maxValue < mediumThreshold;
+        }
+
         public override void onMediumBeat() {
             if (currentLocks > 0) {
                 currentLocks--;
             }
+            if (isEmittingMedium()) {
+                base.onEmit();
+            }
+        }
+
+        private bool isEmittingMedium() {
+            float currentWeight = currentValue / (float)maxValue;
+            return currentWeight < fastThreshold && currentWeight >= mediumThreshold;
         }
 
         public override void onFastBeat() {
-            base.onEmit();
+            if (isEmittingFast()) {
+                base.onEmit();
+            }
+        }
+
+        private bool isEmittingFast() {
+            return currentValue / (float)maxValue >= fastThreshold;
         }
     }
 }
