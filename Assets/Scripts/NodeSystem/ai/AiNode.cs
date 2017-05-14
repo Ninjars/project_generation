d01cc46f7d8e633018fa17d859ed2e36e4021a1f
Jez <jeremy@jeremystevens.net>
2017-05-14 22:34:15 +0100

    improved AiNode target selection, addressing some bugs and refining parameters

--- Assets/Scripts/NodeSystem/ai/AiNode.cs
+++ Assets/Scripts/NodeSystem/ai/AiNode.cs
@@ -44,30 +44,39 @@ namespace Node {
         }
 
         /**
-         * get normalised fraction balancing distance and current value difference, 
-         * prefering close hostlie nodes that can be easily captured
-         * or distant weak friendly nodes
+         * get normalised fraction balancing distance and current value difference:
+         * prefer nodes that are weaker
+         * prefer close hostile nodes
+         * prefer distant friendly nodes
+         * prefer neutral nodes over enemy nodes over friendly nodes
          * 
-         * we don't want to connect to friendly nodes that have already connected to us though!
-         * we don't want to connect to friendly nodes that are maxed out either!
+         * avoid friendly nodes that are maxed out
+         * avoid friendly nodes that are connected to this node
+         * avoid hostile nodes that have greater current value than this node
          */
         private float getWeightingForNodeTarget(GameNode queryNode, GameNode targetNode) {
             bool isFriendly = queryNode.ownerId == targetNode.ownerId;
             if (isFriendly && targetNode.hasConnection(queryNode)) {
                 return -0.5f;
             }
-
-            float normalisedRange = (Vector3.Magnitude(targetNode.getPosition() - queryNode.getPosition()) / GameManager.nodeConnectionRange);
-            if (!isFriendly) {
-                normalisedRange = 1f / normalisedRange;
-            }
-            float captureRatio = Mathf.Min(0, Mathf.Max(1, 1f / (targetNode.currentValue / targetNode.maxValue)));
             if (isFriendly && targetNode.currentValue == targetNode.maxValue) {
-                captureRatio = -0.5f;
+                return -0.5f;
             }
             bool isNeutral = targetNode.ownerId == GameManager.NEUTRAL_PLAYER_ID;
-            float ownershipModifier = isFriendly ? -1 : isNeutral ? 1 : 0;
-            return normalisedRange + captureRatio + ownershipModifier;
+            if (!isNeutral && !isFriendly && targetNode.currentValue > queryNode.currentValue) {
+                return 0;
+            }
+
+            float distanceFactor = Mathf.Min(1, Mathf.Max(0, Vector3.Distance(targetNode.getPosition(), queryNode.getPosition()) / GameManager.nodeConnectionRange));
+            if (!isFriendly) {
+                distanceFactor = 1f - distanceFactor;
+            }
+
+            float weaknessFactor = Mathf.Min(1, Mathf.Max(0, 1f - (targetNode.currentValue / (float) targetNode.maxValue)));
+
+            float alignmentFactor = isFriendly ? 0 : isNeutral ? 1 : 0.5f;
+            float val = 0.15f * distanceFactor +  0.5f * weaknessFactor + 0.35f * alignmentFactor;
+            return val;
         }
     }
 }
