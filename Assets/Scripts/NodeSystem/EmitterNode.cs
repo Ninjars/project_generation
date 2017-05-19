f03729076a6a9378fae9d674502a3cc7cdee52c1
Jez <jeremy@jeremystevens.net>
2017-05-19 20:25:45 +0100

    removed variable rate of emission - nodes will now emit at full speed until they reach their reserve

--- Assets/Scripts/NodeSystem/EmitterNode.cs
+++ Assets/Scripts/NodeSystem/EmitterNode.cs
@@ -6,12 +6,6 @@ namespace Node {
         [Tooltip("Each hostile packet received will add one lock that is cleared every medium beat, up to this many locks")]
         public int maxMediumBeatLockDuration = 3;
         [Range(0f, 1f)]
-        [Tooltip("% above which emitter will emit at fastest speed")]
-        public float fastThreshold = 0.66f;
-        [Range(0f, 1f)]
-        [Tooltip("% above which emitter will emit at medium speed")]
-        public float mediumThreshold = 0.33f;
-        [Range(0f, 1f)]
         [Tooltip("% below which nothing will be emitted")]
         public float reserveThreshold = 0.2f;
 
@@ -35,25 +29,10 @@ namespace Node {
                 changeValue(1);
                 nodeUi.hasUpdate();
             }
-            if (isEmittingSlow()) {
-                base.onEmit();
-            }
-        }
-
-        private bool isEmittingSlow() {
-            float currentWeight = currentValue / (float)maxValue;
-            return currentWeight < mediumThreshold && currentWeight > reserveThreshold;
         }
 
         public override void onMediumBeat() {
-            if (isEmittingMedium()) {
-                base.onEmit();
-            }
-        }
-
-        private bool isEmittingMedium() {
-            float currentWeight = currentValue / (float)maxValue;
-            return currentWeight < fastThreshold && currentWeight >= mediumThreshold;
+            // nothing to see here
         }
 
         public override void onFastBeat() {
@@ -63,7 +42,7 @@ namespace Node {
         }
 
         private bool isEmittingFast() {
-            return currentValue / (float)maxValue >= fastThreshold;
+            return currentValue / (float)maxValue >= reserveThreshold;
         }
     }
 }
