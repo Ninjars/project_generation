e0d9c74cd118e1e6005135ede1304371e00e4839
Jez <jeremy@jeremystevens.net>
2017-05-21 19:59:49 +0100

    refactored NodeUi to consolidate all the functionality of both its former abstract self and SegmentUi, as that was the only implementation

--- Assets/Scripts/NodeSystem/ui/NodeUI.cs
+++ Assets/Scripts/NodeSystem/ui/NodeUI.cs
@@ -1,17 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Node {
     [RequireComponent(typeof(GameNode))]
-    public abstract class NodeUI : MonoBehaviour {
+    public class NodeUI : MonoBehaviour {
         public Material activeValueMaterial;
         public Material passiveValueMaterial;
 
         [Range(0.1f, 3f)]
         public float radius = 1.0f;
+        public float width = 0.25f;
+        public float segmentSeparationRadians = 0.05f;
 
-        protected bool shouldUpdate = true;
-        protected GameObject uiRoot;
-        protected GameNode gameNode;
+        private bool shouldUpdate = true;
+        private GameObject uiRoot;
+        private GameNode gameNode;
+        private List<MeshRenderer> segmentRenderers;
 
         void Awake() {
             gameNode = GetComponent<GameNode>();
@@ -20,10 +24,7 @@ namespace Node {
             uiRoot.transform.position = gameObject.transform.position;
             uiRoot.name = "uiRoot";
             uiRoot.transform.Rotate(Vector3.right, 90);
-            init();
         }
-
-        protected virtual void init() { }
 
         void Start() {
             updateRenderer();
@@ -37,6 +38,66 @@ namespace Node {
             shouldUpdate = true;
         }
 
-        public abstract void updateRenderer();
+        private void updateRenderer() {
+            if (!shouldUpdate) {
+                return;
+            }
+            if (segmentRenderers == null) {
+                segmentRenderers = createSegments(radius, width, gameNode.maxValue);
+            }
+            for (int i = 0; i < segmentRenderers.Count; i++) {
+                Material segmentMaterial;
+                if (i < gameNode.getOwnerValue()) {
+                    segmentMaterial = activeValueMaterial;
+                } else {
+                    segmentMaterial = passiveValueMaterial;
+                }
+                segmentRenderers[i].GetComponent<MeshRenderer>().material = segmentMaterial;
+            }
+            shouldUpdate = false;
+        }
+
+        private List<MeshRenderer> createSegments(float radius, float width, int count) {
+            List<int> triangles = new List<int>() {
+                    0, 1, 2,
+                    1, 3, 2
+                };
+            float segmentArc = (2 * Mathf.PI - (segmentSeparationRadians * count)) / count;
+            float angle = 0;
+            List<MeshRenderer> segments = new List<MeshRenderer>();
+            for (int i = 0; i < count; i++) {
+                List<Vector3> vertices = createSegmentVertices(radius, angle, angle + segmentArc, width);
+                Mesh segmentMesh = new Mesh();
+                segmentMesh.vertices = vertices.ToArray();
+                segmentMesh.triangles = triangles.ToArray();
+                segmentMesh.RecalculateNormals();
+
+                GameObject segmentObject = new GameObject();
+                segmentObject.transform.SetParent(uiRoot.transform);
+                segmentObject.transform.localPosition = Vector3.zero;
+                segmentObject.transform.rotation = uiRoot.transform.rotation;
+                MeshFilter meshFilter = segmentObject.AddComponent<MeshFilter>();
+                meshFilter.mesh = segmentMesh;
+                MeshRenderer meshRenderer = segmentObject.AddComponent<MeshRenderer>();
+
+                segments.Add(meshRenderer);
+                angle += segmentArc + segmentSeparationRadians;
+            }
+            return segments;
+        }
+
+        private List<Vector3> createSegmentVertices(float radius, float startAngle, float endAngle, float width) {
+            Vector3 a = calculateVector(startAngle, radius);
+            Vector3 b = calculateVector(startAngle, radius + width);
+            Vector3 c = calculateVector(endAngle, radius);
+            Vector3 d = calculateVector(endAngle, radius + width);
+            return new List<Vector3>() { a, b, c, d };
+        }
+
+        private Vector3 calculateVector(float angle, float radius) {
+            float x = Mathf.Sin(angle) * radius;
+            float y = Mathf.Cos(angle) * radius;
+            return new Vector3(x, y, 0);
+        }
     }
 }
---
+++ Assets/Scripts/NodeSystem/ui/NodeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Node {
+    public struct PlayerMaterialViewModel {
+        public readonly Material material;
+        public readonly int count;
+
+        public PlayerMaterialViewModel(Material material, int count) {
+            this.material = material;
+            this.count = count;
+        }
+    }
+
+    public struct NodeViewModel {
+        public readonly int maxValue;
+        public readonly List<PlayerMaterialViewModel> valueModel;
+
+        public NodeViewModel(int maxValue, List<PlayerMaterialViewModel> valueModel) {
+            this.maxValue = maxValue;
+            this.valueModel = valueModel;
+        }
+    }
+}
